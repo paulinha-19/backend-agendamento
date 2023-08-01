7e8075e4a60591341701bc242c22c97bff987ebd
Paulinha <paulafabianasoares@gmail.com>
2023-07-31 21:37:07 -0300

    feat: add das rotas forgot, /reset  /password e /auth para privar rota

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const authController = require("../controllers/authController");
-const userController = require("../controllers/userController");
 const router = express.Router();
 const middlewareValidate = require("../middleware/validationSchema");
 const userSchema = require("../validation/userSchema");
-const updateUserSchema = require("../validation/updateUserSchema");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.get("/", (_, res) => {
   return res.json("Bem vindo a API ACME");
+});
+router.get("/auth", authMiddleware(["admin", "patient"]), async (req, res) => {
+  return res.json({
+    message: "You have access to this protected route",
+    user: req.userId,
+    role: req.role,
+  });
 });
 // router.get("/users", userController.getAllUsers);
 // router.get("/user/:id", userController.getOneUser);
@@ -16,10 +22,10 @@ router.post(
   middlewareValidate(userSchema),
   authController.registerUser
 );
-router.post(
-  "/login",
-  authController.loginUser
-);
+router.post("/login", authController.loginUser);
+router.post("/forgot-password", authController.forgotPassword);
+router.post("/reset-password", authController.resetPassword)
+
 // router.delete("/user/:id", userController.deleteUser);
 // router.patch(
 //   "/user/:id",
