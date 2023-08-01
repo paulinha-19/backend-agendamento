ad04d4b3e9de467a5f8d6458a9cb0d7d1aa021a9
Paulinha <paulafabianasoares@gmail.com>
2023-07-31 21:23:45 -0300

    feat(refactor): Add das controllers forgotPassword e resetPassword. Refatoração do loginUser e registerUser

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,10 @@
 const userModel = require("../models/userSchema");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const generateToken = require("../utils/generateToken");
 
 const authController = {
   registerUser: async (req, res) => {
-    const { nome, username, email, password, cpf, sexo, birth } = req.body;
+    const { nome, username, email, cpf } = req.body;
     try {
       const cpfExists = await userModel.findOne({ cpf });
       const emailExists = await userModel.findOne({ email });
@@ -15,25 +15,17 @@ const authController = {
           message: `O username: ${username} e/ou email: ${email} e/ou cpf: ${cpf} já existe no banco de dados. `,
         });
       }
-      const hashedPassword = await bcrypt.hash(password, 10);
-      await userModel.create({
-        nome,
-        username,
-        email,
-        password: hashedPassword,
-        cpf,
-        sexo,
-        birth,
-      });
+      const user = await userModel.create(req.body);
       return res.status(201).json({
         success: true,
         message: `O usuário ${nome} foi registrado!`,
+        token: generateToken({ id: user._id }),
       });
     } catch (error) {
       console.error(error.message);
       return res.status(500).json({
         success: false,
-        message: error.message,
+        message: error,
       });
     }
   },
@@ -41,32 +33,79 @@ const authController = {
     const { email, password } = req.body;
     try {
       if (!email || !password) {
-        return res.json({ message: "All fields are required" });
+        return res.status(400).json({ message: "All fields are required" });
       }
-      const user = await userModel.findOne({ email });
-      if (!user) {
-        return res.status(401).json({ message: "Email not found" });
-      }
-      const passwordMatch = await bcrypt.compare(password, user.password);
-      if (!passwordMatch) {
-        return res.status(401).json({ message: "Incorrect password" });
-      }
-      const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
-        expiresIn: "1h",
-      });
+      const user = await userModel.findOne({ email }).select("+password");
+      if (!user)
+        return res
+          .status(400)
+          .json({ message: "Email or password invalid", success: false });
+
+      if (!(await bcrypt.compare(password, user.password)))
+        return res.status(400).json({ message: "Incorrect password" });
       res.status(200).json({
-        token: `Bearer ${token}`,
-        message: "User logged in successfully",
+        token: generateToken({ id: user._id }),
+        message: `User ${user.nome} logged in successfully`,
         success: true,
         role: user.role,
         nome: user.nome,
-        email: user.email,
       });
     } catch (error) {
-      res
-        .status(500)
-        .json({ message: "Something went wrong", error, success: false });
+      res.status(500).json({ message: "Login error", error, success: false });
       next(error);
+    }
+  },
+  forgotPassword: async (req, res) => {
+    const { email } = req.body;
+    try {
+      const user = await userModel.findOne({ email });
+      if (!email || !user)
+        return res
+          .status(400)
+          .json({ message: "Email not found or invalid", success: false });
+    } catch (error) {
+      res.status(400).json({
+        message: "Erro on forgot password, try again",
+        error,
+        success: false,
+      });
+      next(error);
+    }
+  },
+  resetPassword: async (req, res) => {
+    const { email, password, token } = req.body;
+    try {
+      const user = await userModel
+        .findOne({ email })
+        .select("+passwordResetToken passwordResetExpires");
+      if (!user)
+        return res
+          .status(400)
+          .json({ message: "User not found", success: false });
+
+      if (token !== user.passwordResetToken)
+        return res
+          .status(400)
+          .json({ message: "Token invalid", success: false });
+
+      const now = new Date();
+      if (now > user.passwordResetExpires)
+        return res
+          .status(400)
+          .json({ message: "Token expired", success: false });
+
+      user.password = password;
+      await user.save();
+
+      return res
+        .status(200)
+        .json({ message: "Password reseted", success: true });
+    } catch (error) {
+      res.status(400).json({
+        message: "Erro on reset password, try again",
+        error,
+        success: false,
+      });
     }
   },
 };
