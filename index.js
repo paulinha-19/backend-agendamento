cb52a880b0327d7ec8e8265947752b88768cb0f7
Paulinha <paulafabianasoares@gmail.com>
2023-07-31 21:37:50 -0300

    modificação em index.js, package.json, userSchema em validation

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 const conn = require("./db/conn");
 const routes = require("./routes/router");
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.json());
 conn();
 app.use("/api", routes);
+app.use("/auth", cors());
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
